41ddd522165d9ed6c172474a49e48871cbfe67e5
thestriver <wannabeamumin@gmail.com>
2021-01-23 18:19:51 -0700

    found why mongodb wasnt connecting

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,10 +9,17 @@ setupMiddleWare(app)
 setupDatabase()
     .then((client) => {
         console.log(client)
+
         app.listen(4000 , () => {
             console.log('Server started on port 4000')
         })
     })
     .catch(console.error)
+
+
+//for mongodb to work it has to be installed locally. I used brew as it was easier
+//https://docs.mongodb.com/manual/tutorial/install-mongodb-on-os-x/
+
+
 
 
